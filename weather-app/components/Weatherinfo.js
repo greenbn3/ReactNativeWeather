e295cb37ff64d3c813b669fc622a0b61d07e52df
Noah Greenbaum <greenbn3@miamioh.edu>
2024-07-08 11:27:26 -0400

    Styling page, using multiple items from API, formatting Text, using images

--- weather-app/components/Weatherinfo.js
+++ weather-app/components/Weatherinfo.js
@@ -1,19 +1,49 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
 
-
-export default function Weatherinfo({currentWeather}) {
-const {main: {temp}} = currentWeather
+export default function Weatherinfo({ currentWeather }) {
+  const {
+    main: { temp, feels_like, temp_min, temp_max },
+    weather: [details],
+    name,
+  } = currentWeather;
+  const { icon, main, description } = details;
+  const iconURL = `http://openweathermap.org/img/wn/${icon}@4x.png`;
 
   return (
-    <View style={styles.Weatherinfo}>
-        <Text> {temp} </Text>
-    </View>
-  )
+    <>
+      <View style={styles.Weatherinfo}>
+        <Text style={styles.textHeaders}> Weather Info for {`${name}\n`} </Text>
+        <Text style={styles.textBody}> {`${description}\n`} </Text>
+      </View>
+      <View style={styles.Weatherinfo}>
+        <Text> Current Temperature : {`${temp}\u00B0F`}</Text>
+        <Text> Feels Like : {`${feels_like}\u00B0F`}</Text>
+        <Text> Today's High : {`${temp_max}\u00B0F`}</Text>
+        <Text> Today's Low : {`${temp_min}\u00B0F`}</Text>
+        <Image style={styles.weatherIcon} source={{ uri: iconURL }} />
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    Weatherinfo: {
-        alignItems: 'center'
-    }
-})
+  Weatherinfo: {
+    alignItems: "center",
+  },
+  weatherIcon: {
+    width: 100,
+    height: 100,
+  },
+  textHeaders: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  textBody: {
+    textAlign: "center",
+    fontSize: 16,
+    fontStyle: "italic",
+    textTransform: "capitalize",
+  },
+});
